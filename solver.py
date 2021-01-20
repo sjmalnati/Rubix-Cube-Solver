a6dc2730e0d4c17dd9a664f75065ba082e74dafb
Sam Malnati <sjmalnati@hotmail.com>
2021-01-19 21:29:05 -0500

    rotate skeleton

--- solver.py
+++ solver.py
@@ -1,1 +1,22 @@
 #rubix cube solver
+#0 is front
+#1 is R
+#2 is Top
+#3 is L
+#4 is Bottom
+#5 is Under
+
+def rotate(cube, turnType):
+    if turnType == 'F':
+        #front turn
+        
+    if turnType == 'R':
+        #right turn
+    if turnType == 'L':
+        #left turn
+    if turnType == 'B':
+        #back turn
+    if turnType == 'U':
+        #under turn
+    if turnType == 'T':
+        #top turn
