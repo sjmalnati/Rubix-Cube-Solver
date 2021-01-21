3696659a8ff0fa611ce7e7e3ded0c9e61f0d79e5
Shivank Agrawal <shivankagrawal10@gmail.com>
2021-01-20 21:17:29 -0500

    fixed rotate

--- solver.py
+++ solver.py
@@ -18,82 +18,84 @@ class create_cube():
                     temp[i][j][k] = i
         self.cube=temp
     
-    def rotate(self,turnType):
+    def rotate(cube,turnType):
         temp_cube = copy.deepcopy(self.cube)
         if turnType == 'F':
             #front
             main_face = 0
-            self.cube[2,2,:] = temp_cube[1,:,2]
-            self.cube[3,:,0] = temp_cube[2,2,:]
-            self.cube[4,0,:] = temp_cube[3,:,0]
-            self.cube[1,:,2] = temp_cube[4,0,:]
+            cube[2,2,:] = temp_cube[1,:,2]
+            cube[3,:,0] = temp_cube[2,2,:]
+            cube[4,0,:] = temp_cube[3,:,0]
+            cube[1,:,2] = temp_cube[4,0,:]
         if turnType == 'R':
             #right turn
             main_face = 1
-            self.cube[2,:,2] = temp_cube[0,:,2]
-            self.cube[5,:,2] = temp_cube[2,:,2]
-            self.cube[4,:,2] = temp_cube[5,:,2]
-            self.cube[0,:,2] = temp_cube[4,:,2]
+            cube[2,:,2] = temp_cube[0,:,2]
+            cube[5,:,2] = temp_cube[2,:,2]
+            cube[4,:,2] = temp_cube[5,:,2]
+            cube[0,:,2] = temp_cube[4,:,2]
         if turnType == 'L':
             #left turn
             main_face = 3
-            self.cube[2,:,0] = temp_cube[5,:,0]
-            self.cube[0,:,0] = temp_cube[2,:,0]
-            self.cube[4,:,0] = temp_cube[0,:,0]
-            self.cube[5,:,0] = temp_cube[4,:,0]
+            cube[2,:,0] = temp_cube[5,:,0]
+            cube[0,:,0] = temp_cube[2,:,0]
+            cube[4,:,0] = temp_cube[0,:,0]
+            cube[5,:,0] = temp_cube[4,:,0]
         if turnType == 'B':
             #back turn
             main_face = 5
-            self.cube[4,2,:] = temp_cube[1,:,0]
-            self.cube[3,2,:] = temp_cube[4,2,:]
-            self.cube[2,0,:] = temp_cube[3,2,:]
-            self.cube[1,:,0] = temp_cube[2,0,:]
+            cube[4,2,:] = temp_cube[1,:,0]
+            cube[3,2,:] = temp_cube[4,2,:]
+            cube[2,0,:] = temp_cube[3,2,:]
+            cube[1,:,0] = temp_cube[2,0,:]
         if turnType == 'U':
             #under turn
             main_face = 4
-            self.cube[0,2,:] = temp_cube[1,2,:]
-            self.cube[3,2,:] = temp_cube[0,2,:]
-            self.cube[5,0,:] = temp_cube[3,2,:]
-            self.cube[1,:,2] = temp_cube[5,0,:]
+            cube[0,2,:] = temp_cube[1,2,:]
+            cube[3,2,:] = temp_cube[0,2,:]
+            cube[5,0,:] = temp_cube[3,2,:]
+            cube[1,:,2] = temp_cube[5,0,:]
         if turnType == 'T':
             #top turn
             main_face = 2
-            self.cube[5,2,:] = temp_cube[1,0,:]
-            self.cube[3,0,:] = temp_cube[5,2,:]
-            self.cube[0,0,:] = temp_cube[3,0,:]
-            self.cube[1,0,:] = temp_cube[0,0,:]
+            cube[5,2,:] = temp_cube[1,0,:]
+            cube[3,0,:] = temp_cube[5,2,:]
+            cube[0,0,:] = temp_cube[3,0,:]
+            cube[1,0,:] = temp_cube[0,0,:]
         temp_side = np.zeros((3,3),dtype=int)
-        for i,x in enumerate(self.cube[main_face]):
-            for j,y in enumerate(self.cube[main_face][i]):
-                temp_side[i][j] = self.cube[main_face][(2-j)%3][i]
-        self.cube[main_face] = temp_side
+        for i,x in enumerate(cube[main_face]):
+            for j,y in enumerate(cube[main_face][i]):
+                temp_side[i][j] = cube[main_face][(2-j)%3][i]
+        cube[main_face] = temp_side
         #counter clockwise rotation?
         #cube[faces[0]][2][:] = temp_cube[faces[1]][:][2]
         #cube[faces[1]][:][0] = temp_cube[faces[2]][2][:]
         #cube[faces[2]][0][:] = temp_cube[faces[3]][:][0]
         #cube[faces[3]][:][2] = temp_cube[faces[0]][0][:]
         
-        return(self.cube)
+        return(cube)
 
     def shuffle(self):
         l=['F','B','R','L','T','U']
         for x in range(20):
             i = random.randint(0,5)
             self.cube=self.rotate(l[i])
-
+    
     def solve(cube):
         #breadth first search
         queue = []
         queue.enqueue(cube)
         l=['F','B','R','L','T','U']
-        solved = np.array([][][][][][])
+        solved = create_cube()
+        solved=solved.cube
         while queue:
             curr = queue.pop(0)
             for t in l:
                 turn = rotate(t)
-                if turn == 
+                if turn == solved
+                    return 
                 queue.enqueue(turn)
-                
+
 first=create_cube()
 print(first.cube)
 first.shuffle()
